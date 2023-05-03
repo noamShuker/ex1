191b1a0395c2d315050fdccac8b3c12e74d57200
noamShuker <noam.shuker@campus.technion.ac.il>
2023-05-03 17:35:16 +0300

    Implement CreateQueue

---
+++ IsraeliQueue.c
@@ -0,0 +1,60 @@
+//
+// Created by noams on 5/3/2023.
+//
+#include "IsraeliQueue.h"
+#include <stdlib.h>
+
+typedef struct Node // Must include:
+{
+    void* value;
+    struct Node* next;
+    int friends;
+    int rivels;
+}Node;
+
+typedef struct IsraeliQueue_t
+{
+    Node* head;
+    Node* tail;
+    FriendshipFunction* friendFunction;
+    ComparisonFunction compareFunction;
+    int friendship_th;
+    int rivalry_th;
+
+};
+
+int friendArraySize (FriendshipFunction * friendshipFunction);
+
+IsraeliQueue IsraeliQueueCreate(FriendshipFunction * friendshipFunction, ComparisonFunction comparisonFunction, int
+friendship_th , int rivalry_th)
+{
+    if (comparisonFunction == NULL || friendship_th == NULL || rivalry_th == NULL || friendshipFunction == NULL)
+    {
+        return NULL;
+    }
+
+    IsraeliQueue newQueue = malloc(sizeof (*newQueue));
+    if (newQueue == NULL)
+    {
+        return NULL;
+    }
+    newQueue->compareFunction = comparisonFunction;
+    newQueue->rivalry_th = rivalry_th;
+    newQueue->friendship_th = friendship_th;
+    newQueue->head = NULL;
+    newQueue->tail = NULL;
+    int arrLen = friendArraySize(friendshipFunction+1);
+    FriendshipFunction* ffarr = malloc(arrLen * sizeof(*ffarr));
+    for (int i = 0; i < arrLen; ++i)
+    {
+        newQueue->friendFunction[i] = friendshipFunction[i];
+    }
+
+}
+
+int friendArraySize (FriendshipFunction * friendshipFunction)
+{
+    int i = 0;
+    for (i = 0; friendshipFunction[i] != NULL; ++i);
+    return i;
+}
